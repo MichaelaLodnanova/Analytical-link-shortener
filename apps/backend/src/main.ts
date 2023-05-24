62630125ab0863c12651b75e560bea6033cb4766
Tomáš Falešník <tomas@falesnik.net>
2023-05-24 09:45:37 +0200

    feat: initial project structure

---
+++ apps/backend/src/main.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import cors from 'cors';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+const api = express();
+const port = process.env.BACKEND_PORT ?? 4000;
+
+api.use(express.json());
+api.use(cors());
+
+api.listen(port, () => console.log(`[Backend] is listening on port ${port}`));
