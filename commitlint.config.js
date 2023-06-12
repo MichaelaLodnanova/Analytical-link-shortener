b25b8f328d109fe3485e60c204105a053e579667
Tomáš Falešník <tomas@falesnik.net>
2023-06-12 13:15:42 +0200

    chore: enforce conventional commit scopes

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,10 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-enum': [
+      2,
+      'always',
+      ['model', 'auth', 'be', 'fe', 'common', 'test'],
+    ],
+  },
+};
