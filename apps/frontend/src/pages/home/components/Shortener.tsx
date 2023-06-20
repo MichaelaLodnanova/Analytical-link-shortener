654d23fde1713f2db385b740111b198d4de7521e
xlodnan <xlodnan@fi.muni.cz>
2023-06-20 21:06:39 +0200

    feat: shortener styles added

--- apps/frontend/src/pages/home/components/Shortener.tsx
+++ apps/frontend/src/pages/home/components/Shortener.tsx
@@ -1,5 +1,15 @@
 import { useState, ChangeEvent, FormEvent } from 'react';
-import { Input, Button, Flex, useBreakpointValue } from '@chakra-ui/react';
+import {
+  Input,
+  Button,
+  Flex,
+  useBreakpointValue,
+  Card,
+  CardHeader,
+  Heading,
+  CardBody,
+  CardFooter,
+} from '@chakra-ui/react';
 import BoxItem from './BoxItem';
 
 export default function Shortener() {
@@ -17,26 +27,35 @@ export default function Shortener() {
   const isMobile = useBreakpointValue({ base: true, md: false });
 
   return (
-    <BoxItem color="#8458b3">
+    <BoxItem color="#a088bd">
       <form onSubmit={handleSubmit}>
-        <Flex
-          justifyContent={isMobile ? 'center' : 'flex-start'}
-          alignItems="center"
-          direction={isMobile ? 'column' : 'row'}
-        >
-          <Input
-            type="url"
-            placeholder="Enter URL"
-            value={url}
-            onChange={handleChange}
-            backgroundColor="white"
-            width={isMobile ? '100%' : '2xl'}
-            mb={isMobile ? 2 : 0}
-          />
-          <Button type="submit" colorScheme="twitter" ml={isMobile ? 0 : 2}>
-            Shorten
-          </Button>
-        </Flex>
+        <Card>
+          <CardHeader>
+            <Heading color={'blackAlpha.700'}>Shorten with us!</Heading>
+          </CardHeader>
+          <CardBody>
+            <Flex
+              justifyContent={isMobile ? 'center' : 'flex-start'}
+              alignItems="center"
+              direction={isMobile ? 'column' : 'row'}
+            >
+              <Input
+                type="url"
+                placeholder="Enter URL"
+                value={url}
+                onChange={handleChange}
+                backgroundColor="white"
+                width={isMobile ? '100%' : '2xl'}
+                mb={isMobile ? 2 : 0}
+              />
+            </Flex>
+          </CardBody>
+          <CardFooter>
+            <Button type="submit" colorScheme="purple" ml={isMobile ? 0 : 2}>
+              Shorten
+            </Button>
+          </CardFooter>
+        </Card>
       </form>
     </BoxItem>
   );
