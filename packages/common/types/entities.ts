f05c16acf891505e0115ad67839e00ae0869954f
Matej Turek <469107@mail.muni.cz>
2023-06-21 11:20:55 +0200

    feat: generic response Link type DateLessLink introduced

--- packages/common/types/entities.ts
+++ packages/common/types/entities.ts
@@ -1,4 +1,6 @@
-import { User } from 'model';
+import { Link, User } from 'model';
 import { OmitDates } from './utils';
 
 export type AnonymizedUser = OmitDates<Omit<User, 'passwordHash'>>;
+
+export type DateLessLink = OmitDates<Link>;
